2e456e00b103c810101ee7d13dd8c91799d76a65
Anton Shevchuk <antonshevchuk@gmail.com>
2022-10-28 12:10:02 +0300

    Fixed Tab title

--- WME-E40.user.js
+++ WME-E40.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WME E40 Geometry
-// @version      0.5.4
+// @version      0.5.5
 // @description  Setup POI geometry properties in one click
 // @license      MIT License
 // @author       Anton Shevchuk
@@ -14,7 +14,7 @@
 // @require      https://greasyfork.org/scripts/450160-wme-bootstrap/code/WME-Bootstrap.js?version=1090054
 // @require      https://greasyfork.org/scripts/452563-wme/code/WME.js?version=1101598
 // @require      https://greasyfork.org/scripts/450221-wme-base/code/WME-Base.js?version=1101617
-// @require      https://greasyfork.org/scripts/450320-wme-ui/code/WME-UI.js?version=1101616
+// @require      https://greasyfork.org/scripts/450320-wme-ui/code/WME-UI.js?version=1110180
 // ==/UserScript==
 
 /* jshint esversion: 8 */
